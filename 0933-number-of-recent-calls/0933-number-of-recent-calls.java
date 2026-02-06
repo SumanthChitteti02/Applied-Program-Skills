6572aa58eac7b9ba7645040d47e0e3312a05720a
Sumanth chitteti _02 <vtu26307@veltech.edu.in>
2026-02-06 10:16:59 +0530

    Time: 24 ms (18.41%), Space: 59.2 MB (68.64%) - LeetHub

---
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.java
@@ -0,0 +1,22 @@
+import java.util.Queue;
+import java.util.LinkedList;
+
+class RecentCounter {
+
+    private Queue<Integer> queue;
+
+    public RecentCounter() {
+        queue = new LinkedList<>();
+    }
+
+    public int ping(int t) {
+        queue.offer(t);
+
+        // Remove requests older than t - 3000
+        while (!queue.isEmpty() && queue.peek() < t - 3000) {
+            queue.poll();
+        }
+
+        return queue.size();
+    }
+}
