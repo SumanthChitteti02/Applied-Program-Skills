a68bec545e98348498325222c2454f67010c8675
Sumanth chitteti _02 <vtu26307@veltech.edu.in>
2026-03-07 09:39:50 +0530

    Time: 0 ms (100%), Space: 43 MB (52.45%) - LeetHub

---
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.java
@@ -0,0 +1,13 @@
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while (fast != null && fast.next != null) {
+            slow = slow.next;        // moves 1 step
+            fast = fast.next.next;   // moves 2 steps
+        }
+        
+        return slow; // slow is now at the middle
+    }
+}   
